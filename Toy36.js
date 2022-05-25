f3027ff5052bfa98f5e9a6e8fddb8afda49e4ccd
mae-zung <candylim97@ewhain.net>
2022-05-26 03:50:55 +0900

    2022.5.25

--- Toy36.js
+++ Toy36.js
@@ -5,7 +5,7 @@ function calculateDistance(p1, p2) {
     const yDiff = p2[0] - p1[0];
     const xDiff = p2[1] - p1[1];
     return Math.sqrt(Math.pow(yDiff, 2) + Math.pow(xDiff, 2));
-  }
+}
 
 // [ First Try ]
   
@@ -34,35 +34,65 @@ function calculateDistance(p1, p2) {
 
 // [ Second Try ]
   
+//   const closestPairOfPoints = function (points) {
+
+//     let superMinDis = calculateDistance(points[0],points[1])
+
+//     for(let i=0;i<points.length-1;i++){
+//         //console.log(i)
+//         let minDis = recursive(points[i], points.slice(i+1))
+//         if(superMinDis>minDis) superMinDis = minDis
+//     }
+//     return Math.round(superMinDis*100)
+//   };
+
+//   function recursive (firstEle, points){
+
+//     if(points.length==1) {
+//         return calculateDistance(firstEle, points[0])
+//     }else{
+//         let mid = Math.floor(points.length/2)
+//         // console.log(points.length) --> 2
+//         // console.log(mid) --> 1
+//         let left = points.slice(0,mid) // [1,3]
+//         let right = points.slice(mid) // [2,2]
+//         let leftValue = recursive(firstEle, left)
+//         let rightValue = recursive(firstEle, right)
+        
+//         return Math.min(leftValue,rightValue)
+//         // return 1
+//     }
+//   }
+
+  // [ Third Try ]
+  
   const closestPairOfPoints = function (points) {
 
-    let superMinDis = calculateDistance(points[0],points[1])
-
+    let distances = []
     for(let i=0;i<points.length-1;i++){
-        //console.log(i)
-        let minDis = recursive(points[i], points.slice(i+1))
-        if(superMinDis>minDis) superMinDis = minDis
+        for(let j=i+1;j<points.length;j++){
+            distances.push(calculateDistance(points[i],points[j]))
+        } 
     }
-    return Math.round(superMinDis*100)
+
+    function recursive (array, left, last){
+        if(last-left==1||last==left){ 
+            return Math.min(array[last], array[left])
+        }
+        else{
+            let mid=Math.round((left+last)/2) //2
+            let leftValue = recursive(array,left,mid) // (0,2)
+            let rightValue = recursive(array,mid+1,last) //(3,5)
+    
+            return Math.min(leftValue, rightValue)
+        }
+    }
+
+    let result = recursive(distances,0,distances.length-1) // (0,5)
+    return Math.round(result*100)
   };
 
-  function recursive (firstEle, points){
-
-    if(points.length==1) {
-        return calculateDistance(firstEle, points[0])
-    }else{
-        let mid = Math.floor(points.length/2)
-        // console.log(points.length) --> 2
-        // console.log(mid) --> 1
-        let left = points.slice(0,mid) // [1,3]
-        let right = points.slice(mid) // [2,2]
-        let leftValue = recursive(firstEle, left)
-        let rightValue = recursive(firstEle, right)
-        
-        return Math.min(leftValue,rightValue)
-        // return 1
-    }
-  }
+  
 
 
   //입출력 예시
