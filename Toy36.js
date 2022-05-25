a1470cf788c37d9a3ff66d6a0387249f6cf4e1fd
mae-zung <candylim97@ewhain.net>
2022-05-25 23:51:27 +0900

    2022.5.25

---
+++ Toy36.js
@@ -0,0 +1,98 @@
+//좌표평면 상의 다양한 점들을 입력받아 가장 가까운 두 점 사이의 거리를 리턴해야 합니다.
+
+// 좌표평면 위의 두 점 사이의 거리를 계산하는 함수입니다.
+function calculateDistance(p1, p2) {
+    const yDiff = p2[0] - p1[0];
+    const xDiff = p2[1] - p1[1];
+    return Math.sqrt(Math.pow(yDiff, 2) + Math.pow(xDiff, 2));
+  }
+
+// [ First Try ]
+  
+//   const closestPairOfPoints = function (points) {
+//     // TODO: 여기에 코드를 작성합니다.
+
+//     let superMinDis = calculateDistance(points[0],points[1])
+
+//     for(let i=0;i<points.length-1;i++){
+//         //console.log(i)
+//         let minDis = calculateDistance(points[i],points[i+1])
+//         for(let j=i+2;j<points.length;j++){
+//             // console.log(i+','+j)
+//             let distance = calculateDistance(points[i],points[j])
+//             if(minDis>distance) minDis = distance
+//         }
+//         if(superMinDis>minDis) superMinDis = minDis
+//     }
+
+//     return Math.round(superMinDis*100)
+
+//   };
+
+
+
+
+// [ Second Try ]
+  
+  const closestPairOfPoints = function (points) {
+
+    let superMinDis = calculateDistance(points[0],points[1])
+
+    for(let i=0;i<points.length-1;i++){
+        //console.log(i)
+        let minDis = recursive(points[i], points.slice(i+1))
+        if(superMinDis>minDis) superMinDis = minDis
+    }
+    return Math.round(superMinDis*100)
+  };
+
+  function recursive (firstEle, points){
+
+    if(points.length==1) {
+        return calculateDistance(firstEle, points[0])
+    }else{
+        let mid = Math.floor(points.length/2)
+        // console.log(points.length) --> 2
+        // console.log(mid) --> 1
+        let left = points.slice(0,mid) // [1,3]
+        let right = points.slice(mid) // [2,2]
+        let leftValue = recursive(firstEle, left)
+        let rightValue = recursive(firstEle, right)
+        
+        return Math.min(leftValue,rightValue)
+        // return 1
+    }
+  }
+
+
+  //입출력 예시
+  let points = [
+    [0, 0],
+    [1, 3],
+    [2, 2],
+  ];
+  let output = closestPairOfPoints(points);
+  console.log(output); // --> 141 ([1, 3], [2, 2])
+  /*
+  3 |  x
+  2 |     x
+  1 |       
+  0 | x 
+  ------------
+      0 1 2 3 
+  */
+  
+  points = [[0, 100], [3, 4], [58, 34], [22, 35], [121, 132], [140, 153]]
+  output = closestPairOfPoints(points);
+  console.log(output); // --> 100 ([0, 0], [0, 1])
+  /*
+  5 | x
+  4 | 
+  3 | x
+  2 |     
+  1 | x     
+  0 | x 
+  ------------
+      0 1 2 3 
+  */
+  
